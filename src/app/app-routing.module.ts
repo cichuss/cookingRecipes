8b86e52a2b3a6f017ee49af2fb11fc81b245835e
Hania111 <chococookimonster@gmail.com>
2024-04-22 14:07:10 +0100

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./pages/landing-page/landing-page.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {FooterHeaderMenuLayoutComponent} from "./Layouts/footer-header-menu-layout/footer-header-menu-layout.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {MenuComponent} from "./components/menu/menu.component";
+import {ConnectComponent} from "./pages/connect/connect.component";
+import {LikedMoviesComponent} from "./pages/liked-movies/liked-movies.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: FooterHeaderMenuLayoutComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'connect', component: ConnectComponent },
+      { path: 'liked', component: LikedMoviesComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterHeaderMenuLayoutComponent } from './Layouts/footer-header-menu-layout/footer-header-menu-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LikedMoviesComponent } from './pages/liked-movies/liked-movies.component';
+import { ConnectComponent } from './pages/connect/connect.component';
+import { ShakerComponent } from './pages/shaker/shaker.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterHeaderMenuLayoutComponent,
+    HomeComponent,
+    LikedMoviesComponent,
+    ConnectComponent,
+    ShakerComponent,
+    FooterComponent,
+    MenuComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
