fdee8023cf44db4e40f2a7273244329b91263dc4
Hania111 <chococookimonster@gmail.com>
2024-05-05 23:17:12 +0100

    added list of liked movies

--- src/app/pages/liked-movies/liked-movies.component.ts
+++ src/app/pages/liked-movies/liked-movies.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Movie} from "../../models/movie";
+import {MovieService} from "../../services/movie.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-liked-movies',
   templateUrl: './liked-movies.component.html',
   styleUrl: './liked-movies.component.css'
 })
-export class LikedMoviesComponent {
+export class LikedMoviesComponent implements OnInit{
+  movies:Movie[] = [];
+  title: string | undefined;
+  synopsis: string | undefined;
 
+
+  constructor(
+    private movieService: MovieService,
+    private modal: NgbModal
+  ) {}
+
+  ngOnInit() {
+    this.movieService.getMovies().subscribe((res: Movie[]) => {
+      this.movies = res;
+      //console.log(this.movies);
+      for (let i = 0; i < this.movies.length; i++) {
+        this.loadMovieData(i);
+        console.log(this.movies[i].posterUrl);
+      }
+    });
+
+  }
+
+  loadMovieData(movieNr: number){
+    this.title = this.movies[movieNr].title;
+    this.synopsis = this.movies[movieNr].description;
+
+    if (this.movies[movieNr].posterUrl) {
+      this.movieService.getImageUrl(this.movies[movieNr].posterUrl).subscribe(url => {
+        this.movies[movieNr].posterUrl = url;
+        //console.log(url);
+      }, error => console.error(error));
+    }
+  }
 }
